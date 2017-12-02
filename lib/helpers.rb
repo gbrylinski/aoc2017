94b645b14f0c32caf768c5f022fc52efddb46d6b
Grzegorz Bryliński <gbrylinski@gmail.com>
2017-12-02 14:24:01 +0100

    Update helpers with new file reader

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,6 +1,13 @@
 module AOCHelpers
   def read_line_from(filename)
-    File.readlines(filename).first.chomp
+    read_lines_from(filename).first
+  end
+
+  def read_lines_from(filename)
+    File
+      .readlines(filename)
+      .map(&:chomp)
+      .reject(&:empty?)
   end
 
   def show_solution(day, part, solution)
